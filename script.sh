baef5c04b64a18f7cfc46c174e9f46e29e1bb3e3
Tus <54474594+tuskingcup@users.noreply.github.com>
2023-11-22 17:29:26 +0700

    use script.sh

--- script.sh
+++ script.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-sshpass -p "${{ secrets.PROD_PWD }}" ssh -tt -o StrictHostKeyChecking=no ${{ secrets.PROD_ADMIN }}@${{ secrets.PROD_HOSTNAME }} << EOF
+sshpass -p "${ PROD_PWD }" ssh -tt -o StrictHostKeyChecking=no ${ PROD_ADMIN }@${ PROD_HOSTNAME } << EOF
     cd ~/LC-Deployment
     envsubst < mysql.env.tmpl > mysql.env
     envsubst < backend.env.tmpl > backend.env
-    docker login ghcr.io -u ${{ secrets.TUS_GH_USER }} -p ${{ secrets.GHCR_PAT }}
+    docker login ghcr.io -u ${ TUS_GH_USER } -p ${ GHCR_PAT }
     git pull
     docker-compose pull
     docker-compose up -d
