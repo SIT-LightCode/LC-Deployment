bb51c3eae75a1b24f54faaab23fddf3af8b819ee
Tus <54474594+tuskingcup@users.noreply.github.com>
2023-11-22 17:22:18 +0700

    use script.sh

--- script.sh
+++ script.sh
@@ -2,10 +2,6 @@
 
 sshpass -p "${{ secrets.PROD_PWD }}" ssh -tt -o StrictHostKeyChecking=no ${{ secrets.PROD_ADMIN }}@${{ secrets.PROD_HOSTNAME }} << EOF
     cd ~/LC-Deployment
-    export MYSQL_ROOT_PASSWORD=${{ secrets.MYSQL_PASSWD }}
-    export MYSQL_DATABASE=${{secrets.MYSQL_DATABASE}}
-    export MYSQL_USER=${{secrets.MYSQL_USER}}
-    export MYSQL_PASSWORD=${{secrets.MYSQL_PASSWORD}}
     envsubst < mysql.env.tmpl > mysql.env
     envsubst < backend.env.tmpl > backend.env
     docker login ghcr.io -u ${{ secrets.TUS_GH_USER }} -p ${{ secrets.GHCR_PAT }}
